131baff073adc32458394f2c46eb75c87be4dfe0
hewigovens <360470+hewigovens@users.noreply.github.com>
2024-05-15 11:22:14 +0900

    Update Hero.tsx

--- src/partials/Hero.tsx
+++ src/partials/Hero.tsx
@@ -18,7 +18,7 @@ const Hero = () => (
         <>
           Tokyo-based, crypto-focused, and former <a className="text-cyan-400 hover:underline" href="https://trustwallet.com/">
             Trust Wallet
-          </a> fouding engineer. Blockchains, Cross-platform SDK, mobile apps, and smart contracts are areas of expertise.
+          </a> Founding engineer. Blockchains, Cross-platform SDK, mobile apps, and smart contracts are areas of expertise.
           <br />
           Proficient with major networks (not limited to {' '}
           <img src="/assets/images/btc.svg" alt="BTC" className='h-6 w-6 inline-block mr-1' />
