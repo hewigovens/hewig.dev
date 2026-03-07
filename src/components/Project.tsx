90d7e0341293f4c0ba359b719c4c575f86ee21a4
shiorixbot <258157775+shiorixbot@users.noreply.github.com>
2026-03-07 14:06:06 +0000

    feat: round project icons, add website links to stub pages
    - Add rounded-2xl to Project component img for consistent rounded corners
    - Add "Visit Website" link to Anytime (fourplexlabs.github.io/AnyTime)
    - Add "Visit Website" link to Inspect (fourplexlabs.github.io/Inspect)
    - GhostTile already links to ghosttile.kernelpanic.im

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -16,7 +16,7 @@ const Project = (props: IProjectProps) => (
     <div className="shrink-0">
       <a href={props.link}>
         <img
-          className="h-36 w-36 hover:translate-y-1"
+          className="h-36 w-36 rounded-2xl hover:translate-y-1"
           src={props.img.src}
           alt={props.img.alt}
           loading="lazy"
